0416716a8a9522fc631e30c9d007bac4ce5202bc
Notiavina840 <notiavina840@gmail.com>
2025-12-01 09:04:45 +0300

    Sprint1-debug

--- src/app/controllers/aaaController.java
+++
@@ -1,9 +0,0 @@
-package app.controllers;
-
-import jakarta.servlet.http.HttpServletRequest;
-
-public class aaaController {
-    public String execute(HttpServletRequest request) {
-        return "<h1>Bonjour, c'est la page AAA !</h1>";
-    }
-}
